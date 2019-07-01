cbe41a358091902fcdeae9dcfa4cda6849b2d9a9
erwin snijder <erwinsnijder_4@hotmail.com>
2019-07-01 16:40:26 +0200

    update

--- js/script.js
+++ js/script.js
@@ -54,16 +54,16 @@ let output =`
 <img src="${movie.Poster}" class="thumbnail">
 </div>
 <div class="col-md-8">
-<h2>${movie.Title}</h2>
-<ul class="list-group">
-    <li class="list-group-item"><strong>Release Year:</strong>${movie.Released}</li>
-    <li class="list-group-item"><strong>Genre:</strong>${movie.Genre}</li>
-    <li class="list-group-item"><strong>Actors:</strong>${movie.Actors}</li>
-    <li class="list-group-item"><strong>Rated:</strong>${movie.Rated}</li>
-    <li class="list-group-item"><strong>Language:</strong>${movie.Language}</li>
-    <li class="list-group-item"><strong>Runtime:</strong>${movie.Runtime}</li>
-    <li class="list-group-item"><strong>imdbRating:</strong>${movie.imdbRating}</li>
-    <li class="list-group-item"><strong>Writer:</strong>${movie.Writer}</li>
+<h2>${movie.Title} +(${movie.Year})</h2>
+<ul class="list">
+    <li class="list"><strong>Release Year:</strong>${movie.Released}</li>
+    <li class="list"><strong>Runtime:</strong>${movie.Runtime}</li>
+    <li class="list"><strong>Genre:</strong>${movie.Genre}</li>
+    <li class="list"><strong>Actors:</strong>${movie.Actors}</li>
+    <li class="list"><strong>Rated:</strong>${movie.Rated}</li>
+    <li class="list"><strong>Language:</strong>${movie.Language}</li>
+    <li class="list"><strong>imdbRating:</strong>${movie.imdbRating}</li>
+    <li class="list"><strong>Writer:</strong>${movie.Writer}</li>
 </ul>
 </div>
 </div>
@@ -71,7 +71,10 @@ let output =`
 <div class="well">
 <h3>Plot</h3>
 ${movie.Plot}
-<div>
+<hr>
+<a href="http://imdb.com/title/${movie.imdbID}" target="blank" class= "btn btn-primary">Imdb Page</a>
+
+</div>
 
 `
 $('#movie').html(output);
