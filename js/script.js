d46b64a3ed45b0f4e24c74bbffedfc59b472ecd5
erwin snijder <erwinsnijder_4@hotmail.com>
2019-07-01 10:09:19 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 $(document).ready(() => {
     $('#searchForm').on('submit', (e) => {
         let searchText = $('#searchText').val();
-        getMovies('searchText');        
-        e.preventDefault();
+    getMovies(searchText);
+    e.preventDefault();
     });
 });
 
 function getMovies(searchText){
 console.log(searchText);
+axios.get('https://www.omdbapi.com/?s='+searchText+'&apikey=d62e2d9d')
+.then( (response) => {
+console.log(response);
+})
+.catch( (err) => {
+console.log(err);
+});
+
 }
