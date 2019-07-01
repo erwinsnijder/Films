51720ca9c0bdc92122bc53172910d070bd7803a7
erwin snijder <erwinsnijder_4@hotmail.com>
2019-07-01 11:22:50 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,11 +17,15 @@ $.each(movies, (index, movie) => {
 output += `
 <div class="col-md-3">
 <div class="well text-center">
+<img src="${movie.Poster}">
 <h5>${movie.Title}</h5>
+<a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
 </div>
 </div>
 `;
 });
+
+$('#movies').html(output);
 })
 .catch( (err) => {
 console.log(err);
